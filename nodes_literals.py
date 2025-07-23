b2ea2117de7359a502d34045563c94d055ba69e9
Alexopus <alexopus.soft@gmail.com>
2025-07-23 11:31:21 +0200

    Add ConditioningConcatOptional

--- nodes_literals.py
+++ nodes_literals.py
@@ -118,3 +118,36 @@ class CfgLiteral:
 
     def get_float(self, cfg: float) -> tuple[float,]:
         return (cfg,)
+
+class ConditioningConcatOptional:
+    @classmethod
+    def INPUT_TYPES(cls) -> dict[str, Any]:
+        return {
+            "required": {
+                "conditioning_to": ("CONDITIONING", {"tooltip": "base conditioning to concat to (or pass through, if second is empty)"}),
+            },
+            "optional": {
+                "conditioning_from": ("CONDITIONING", {"tooltip": "conditioning to concat to conditioning_to, if empty, then conditioning_to is passed through unchanged"}),
+            }
+        }
+
+    RETURN_TYPES = ("CONDITIONING",)
+    FUNCTION = "concat"
+    CATEGORY = "conditioning"
+
+    def concat(self, conditioning_to, conditioning_from=None):
+        if conditioning_from is None:
+            return (conditioning_to,)
+
+        out = []
+        if len(conditioning_from) > 1:
+            logging.warning("Warning: ConditioningConcat conditioning_from contains more than 1 cond, only the first one will actually be applied to conditioning_to.")
+
+        cond_from = conditioning_from[0][0]
+        for i in range(len(conditioning_to)):
+            t1 = conditioning_to[i][0]
+            tw = torch.cat((t1, cond_from), 1)
+            n = [tw, conditioning_to[i][1].copy()]
+            out.append(n)
+
+        return (out,)
